99eb7297230c4580f41be5b4daae32af43a6b152
Akash Yadav <akash.yadav@clobz.com>
2025-09-17 15:36:18 +0530

    code updated

---
+++ main.js
@@ -0,0 +1,21 @@
+function updateBusinessType(node){
+    debugger
+    let btnList= document.getElementById("businessType_list");
+    let allBtns= btnList.querySelectorAll("button");
+    allBtns.forEach((btn)=>{
+        if(btn != node){
+        btn.classList.contains("active-business-type") ? btn.classList.remove("active-business-type") : null;
+        const svg = btn.querySelector("svg");
+        if (svg.getAttribute("fill") === "#ffffff") {
+          svg.setAttribute("fill", "#6d6b6b"); 
+        }
+    }
+    })
+    node.classList.contains("active-business-type") ? node.classList.remove("active-business-type") : node.classList.add("active-business-type");;
+    let svg = node.querySelector("svg");
+    if (svg.getAttribute("fill") === "#ffffff") {
+        svg.setAttribute("fill", "#6d6b6b"); 
+    } else {
+        svg.setAttribute("fill", "#ffffff");
+    }
+}
